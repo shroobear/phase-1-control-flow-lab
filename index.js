9c3a8b314d95458ca2d76de249f699ce65a3f6d1
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-03-14 21:07:49 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(footage){
+  if (footage <= 400) {
+    return 'This one is on me!'
+  } else if (footage <= 2000) {
+    return 'That will be twenty bucks.'
+  } else if (footage <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination){
+  return destination === 'NYC' ? ('Ok, sounds good.') : ('No go.');
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipSize){
+  let reply
+
+  switch (tipSize) {
+    case 'generous':
+      reply = 'Thank you so much.'
+      break;
+    case 'not as generous':
+      reply = 'Thank you.'
+      break;
+    default:
+      reply = 'Bye.'
+  }
+  return reply
   // Write your code here!
 }
